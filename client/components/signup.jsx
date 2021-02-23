0c87a51bc352baace8ba850b4c8b10043474c8a2
Mason Keiser <masonksr5@gmail.com>
2021-02-23 20:40:15 +0000

    fixed signup show pass fx

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -18,19 +18,30 @@ const Signup = (props) => {
     }
 
     const passEye = () => {
-        const eye = document.getElementById('eye')
-        const pass = document.getElementById('password')
-        console.log(pass.type)
-        if (eye.classList.contains('show')) {
-            eye.classList.remove('fa-eye-slash')
-            eye.classList.remove('show')
-            pass.type = 'text'
-            eye.classList.add ('fa-eye')
+        const eye1 = document.querySelector('#eye1')
+        const eye2 = document.querySelector('#eye2')
+        const pass1 = document.querySelector('.pass1')
+        const pass2 = document.querySelector('.pass2')
+        if (eye1.classList.contains('show')) {
+            eye1.classList.remove('fa-eye-slash')
+            eye1.classList.remove('show')
+            pass1.type = 'text'
+            eye1.classList.add ('fa-eye')
         } else {
-            eye.classList.add('fa-eye-slash')
-            eye.classList.add('show')
-            pass.type = 'password'
-            eye.classList.remove('fa-eye')
+            eye1.classList.add('fa-eye-slash')
+            eye1.classList.add('show')
+            pass1.type = 'password'
+            eye1.classList.remove('fa-eye')
+        } if (eye2.classList.contains('show')) {
+            eye2.classList.remove('fa-eye-slash')
+            eye2.classList.remove('show')
+            pass2.type = 'text'
+            eye2.classList.add ('fa-eye')
+        } else {
+            eye2.classList.add('fa-eye-slash')
+            eye2.classList.add('show')
+            pass2.type = 'password'
+            eye2.classList.remove('fa-eye')
         }
     }
     
@@ -57,16 +68,16 @@ const Signup = (props) => {
                         <label htmlFor="password">Password:</label>
                         <div className='d-flex'>
                             <span className='fas fa-lock'></span>
-                            <input type="password" placeholder='Enter Password' autoComplete='current-password' name='password' className="" id='password'/>
-                            <span onClick={() => passEye()} id='eye' className='fas fa-eye-slash show'></span>
+                            <input type="password" placeholder='Enter Password' autoComplete='current-password' name='password' className="pass1" id='password'/>
+                            <span onClick={() => passEye(event)} title='1' id='eye1' className='fas fa-eye-slash show'></span>
                         </div>
                     </div>
                     <div className='form-group'>
                         <label htmlFor="password">Re-Enter Password:</label>
                         <div className='d-flex'>
                             <span className='fas fa-lock'></span>
-                            <input type="password" placeholder='Re-Enter Password' autoComplete='current-password' name='password' className="" id='password'/>
-                            <span onClick={() => passEye()} id='eye' className='fas fa-eye-slash show'></span>
+                            <input type="password" placeholder='Re-Enter Password' autoComplete='current-password' name='password' className="pass2" id='password'/>
+                            <span onClick={() => passEye()} title='2' id='eye2' className='fas fa-eye-slash show'></span>
                         </div>
                     </div>
                     <div id="required" className="required"></div>
