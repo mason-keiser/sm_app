f520f952c13f8bb1cf9804cdb22001cad2f2c9c3
Mason Keiser <masonksr5@gmail.com>
2021-02-24 02:12:32 +0000

    backend login/signup apis added

--- server/index.js
+++ server/index.js
@@ -19,6 +19,53 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+//  SIGN UP API POST REQUEST THAT ADDS USER INFO TO DB
+
+app.post('/api/signUp/', (req, res, next) => {
+  const sql = `
+  INSERT INTO "users" ("user_name", "user_password")
+  VALUES                  ($1, $2)
+  RETURNING *;
+  `;
+  const params = [req.body.user_name, req.body.user_password];
+  
+  for (let i = 0; i < params.length; i ++) {
+    if (!params[i]) {
+      return res.status(400).json({ error: 'all signup input forms must be filled' });
+    }
+  }
+  db.query(sql, params)
+    .then(result => {
+      const row = result.rows[0];
+      res.status(201).json(row);
+    });
+});
+
+//  SEARCH DATABASE FOR EXISTING EMAIL AND PASSWORD API GET REQUEST
+
+app.get('/api/login/:user_name/:user_password/', (req, res, next) => {
+  const username = req.params.user_name;
+  const password = req.params.user_password;
+  const sql = `
+  SELECT * FROM "users"
+  WHERE "user_name" = $1 
+  AND "user_password" = $2;
+  `;
+  const params = [username, password];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        return res.status(400).json({ message: `No user information contains: ${username} or ${password}` });
+      } else {
+        return res.status(200).json(result.rows);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
