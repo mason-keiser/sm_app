d0c733b9465fc4d2b98970e7dd784546224160b7
Mason Keiser <masonksr5@gmail.com>
2021-03-17 01:01:11 +0000

    side menu styling started

--- client/components/feed.jsx
+++ client/components/feed.jsx
@@ -12,6 +12,20 @@ const Feed = (props) => {
                     <h4 id='time'>{props.formatAMPM(new Date())}</h4>
                 </div>
             </div>
+            <div className='sideMenu'>
+                <h3 className='menTit mt-3'>üConnect</h3>
+                <div className='profileTag'>
+                    <div className='fas fa-user'></div>
+                    <h4>My Profile</h4>
+                </div>
+                <div>
+                    <h6>Night Mode:</h6>
+                    <div className='modeToggler'>
+
+                    </div>
+                </div>
+                <h6 className='foote'> Mason Keiser  © 2021. All rights reserved.</h6>
+            </div>
             <Bg/>
         </div>
     )
