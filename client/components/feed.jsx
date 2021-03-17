70cd4bc010b2ec392f3ec2a8cfffc2e6e37a275f
Mason Keiser <masonksr5@gmail.com>
2021-03-17 02:48:35 +0000

    cleaned up light mode

--- client/components/feed.jsx
+++ client/components/feed.jsx
@@ -9,8 +9,15 @@ const Feed = (props) => {
         const icon = document.getElementById('modeIcon');
         const bar = document.querySelector('.modeToggler')
         const bg = document.getElementById('tsparticles')
-        console.log(bg.firstChild)
+        const words = document.querySelectorAll('.nm')
+        const userI = document.getElementById('userI')
+
         if (iconHolder.classList.contains('d')) {
+            for (let i = 0; i < words.length; i++ ) {
+                words[i].style.color = 'black'
+                words[i].style.borderColor = 'black'
+            }
+            userI.style.color = 'black'
             iconHolder.style.float = 'right'
             iconHolder.classList.remove('d')
             icon.classList.remove('fa-moon')
@@ -20,15 +27,19 @@ const Feed = (props) => {
             bar.style.background = 'white'
             bg.firstChild.style.backgroundColor="#F5F5F5"
         } else {
+            for (let i = 0; i < words.length; i++ ) {
+                words[i].style.color = 'white'
+                words[i].style.borderColor = 'white'
+            }
+            userI.style.color = 'white'
             iconHolder.style.float = 'left'
             iconHolder.classList.add('d')
             icon.style.color = 'yellow'
             icon.classList.remove('fa-sun')
             icon.classList.add('fa-moon')
-            iconHolder.style.background = '#4F4F4F'
+            iconHolder.style.background = '#262626'
             bar.style.background = 'black'
-            bg.firstChild.style.backgroundColor='#4F4F4F'
-
+            bg.firstChild.style.backgroundColor='#262626'
         }
     }
 
@@ -36,25 +47,25 @@ const Feed = (props) => {
         <div>
             <div className='landingNav'>
                 <div className='dateHolder'>
-                    <h4 className="date2 border-0 m-3">{props.dateBuilder(new Date())}</h4>
-                    <h4 id='time'>{props.formatAMPM(new Date())}</h4>
+                    <h4 className=" nm date2 border-0 m-3">{props.dateBuilder(new Date())}</h4>
+                    <h4 id='time' className='nm'>{props.formatAMPM(new Date())}</h4>
                 </div>
             </div>
             <div className='sideMenu'>
-                <h3 className='menTit mt-3'>üConnect</h3>
+                <h3 className='menTit mt-3 nm'>üConnect</h3>
                 <div className='profileTag'>
-                    <div className='fas fa-user'></div>
-                    <h4>My Profile</h4>
+                    <div id='userI' className='fas fa-user'></div>
+                    <h4 className='nm'>My Profile</h4>
                 </div>
                 <div className='togg d-flex flex-column align-items-center'>
-                    <h6>Night Mode:</h6>
+                    <h6 className='nm'>Night Mode:</h6>
                     <div className='modeToggler'>
                         <div onClick={() => toggler()} className='toggIconHolder'>
                             <span id='modeIcon' className='fas fa-sun'></span>
                         </div>
                     </div>
                 </div>
-                <h6 className='foote'> Mason Keiser  © 2021. All rights reserved.</h6>
+                <h6 className='foote nm'> Mason Keiser  © 2021. All rights reserved.</h6>
             </div>
             <Bg/>
         </div>
