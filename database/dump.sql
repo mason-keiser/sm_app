005cb03d4673b1d1abe7f000bd7c292558dba268
Mason Keiser <masonksr5@gmail.com>
2021-03-23 00:33:57 +0000

    post sql table made

--- database/dump.sql
+++ database/dump.sql
@@ -18,8 +18,11 @@ SET row_security = off;
 
 ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS users_pkey;
 ALTER TABLE IF EXISTS public.users ALTER COLUMN user_id DROP DEFAULT;
+ALTER TABLE IF EXISTS public.posts ALTER COLUMN post_id DROP DEFAULT;
 DROP SEQUENCE IF EXISTS public.users_user_id_seq;
 DROP TABLE IF EXISTS public.users;
+DROP SEQUENCE IF EXISTS public.posts_post_id_seq;
+DROP TABLE IF EXISTS public.posts;
 DROP EXTENSION IF EXISTS plpgsql;
 DROP SCHEMA IF EXISTS public;
 --
@@ -55,6 +58,40 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    post_id integer NOT NULL,
+    user_id integer NOT NULL,
+    post character varying(500) NOT NULL,
+    post_image character varying(100000),
+    likes integer NOT NULL,
+    replies text[]
+);
+
+
+--
+-- Name: posts_post_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.posts_post_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: posts_post_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.posts_post_id_seq OWNED BY public.posts.post_id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -88,10 +125,27 @@ ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
 
 
 --
+-- Name: posts post_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN post_id SET DEFAULT nextval('public.posts_post_id_seq'::regclass);
+
+
+--
 -- Name: users user_id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.posts (post_id, user_id, post, post_image, likes, replies) FROM stdin;
+1	16	New phone who dis?	\N	69	\N
+2	16	Time to go surfing	\N	4	\N
+\.
 
 
 --
@@ -102,14 +156,22 @@ COPY public.users (user_id, user_name, user_password, user_profile_image, user_h
 12	YungMas	$2b$10$rPs4evFu86TCnfa/Hfg8eOyK2mHhLHXed/oNfSScavfTAqL3ssoi6	\N	\N
 14	Yu	$2b$10$NjXYEpiLuTFLHpCO1Qh40ukwSOldm17w67L6JIMQuscKLeKQ85hrC	\N	\N
 16	masonksr	$2b$10$tjE4LKqpgmE1fHn3oL1eD.Uw878xYXmv5QduOka2SyAukTXP913rm	\N	\N
+17	@guest	$2b$10$ltp3aM65GBmy2CKzFTzrNeQA7luwkoMoMxqjYzvora1qtueLEO7TO	\N	\N
 \.
+
+
+--
+-- Name: posts_post_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.posts_post_id_seq', 2, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 16, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 17, true);
 
 
 --
